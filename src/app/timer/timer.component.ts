9da537172c1744a120244719ba64c24d99aa9b10
Arthur <adamgontier898@gmail.com>
2019-09-04 14:02:04 +0300

    init commit

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.sass']
+})
+export class TimerComponent implements OnInit {
+
+  private numericTime: number = 0;
+  private sub: Subscription;
+  private timer: any = interval(1000);
+  private stopped: boolean = true;
+  private paused: boolean = false;
+  private timePause = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.numericTime = 0;
+  }
+
+  wait(){
+    if(this.paused){
+      this.sub = this.timer.subscribe((n: number) => {
+        this.numericTime = (n + this.timePause) * 1000;
+      })
+      this.paused = false;
+    } else{
+      this.sub.unsubscribe();
+      this.timePause = this.numericTime / 1000 + 1;
+      this.paused = true;
+    }
+  }
+
+  stop(){
+    if(!this.stopped){ 
+      this.sub.unsubscribe();
+      this.stopped = true;
+     } 
+    else { 
+      this.numericTime =0;
+      this.sub = this.timer.subscribe((n: number) => {
+        this.numericTime = (n+1) * 1000;
+      })
+      this.stopped = false;
+    }
+  }
+
+  reset(){
+    this.numericTime = 0;
+    if(this.sub !== undefined){
+      this.sub.unsubscribe()
+      this.sub = this.timer.subscribe((n: number) => {
+        this.numericTime = (n+1) * 1000;
+      })
+    }    
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
+
+}
